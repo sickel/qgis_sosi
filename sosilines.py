57a49bb7bb44f13f65a467709926072e95c27ad1
Morten Sickel <morten@sickel.net>
2024-08-27 06:46:42 +0200

    More details

--- sosilines.py
+++ sosilines.py
@@ -1,4 +1,22 @@
+from datetime import datetime
+
 # https://register.geonorge.no/data/documents/sosi-standard-del-1-generell-del_Realisering%20i%20SOSI-format%20og%20GML_v1_del1-2-realiseringsosigml-4_0_.pdf
+# De følgende parameterene må kunne settes utenifra
+dataeier = 'NORSAR' 
+dataprodusent = 'NORSAR'   
+oppdateringsdato = '20240815'
+objtype = 'Kabelgrøft'
+objektkatalog = ''
+datauttaksdato = datetime.now().strftime("%Y%m%d%H%M%S")
+sosiversjon = '4'
+layer = qgis.utils.iface.activeLayer()
+crsid = layer.crs().authid()
+name = layer.name()
+extent = layer.extent()
+filename = f"{name}.sos"
+unit = 0.01
+objectid = 0
+sosiversjon = '4.5'
 
 crscodes = {
 'EPSG:25831': 21,
@@ -21,16 +39,7 @@ try:
 except:
     crscode = 99
     print('TRANSSYS not implemented yet')
-dataeier = 'NORSAR' 
-dataprodusent = 'NORSAR'   
-layer = qgis.utils.iface.activeLayer()
-crsid = layer.crs().authid()
-name = layer.name()
-extent = layer.extent()
-filename = f"{name}.sos"
-unit = 0.01
-objectid = 0
-objtype = 'Kabelgrøft'
+
 with open (filename,'w') as outfile:
     hode=f""".HODE
 ..TEGNSETT UTF-8
@@ -43,22 +52,30 @@ with open (filename,'w') as outfile:
 ...MAX-NØ {round(extent.yMaximum()/unit)*unit} {round(extent.xMaximum()/unit)*unit}
 ..SOSI-NIVÅ 4
 ..EIER {dataeier}
+..OBJEKTKATALOG {objektkatalog}
 ..PRODUSENT {dataprodusent}
-..SOSI-VERSJON 4.5"""
+..SOSI-VERSJON {sosiversjon}"""
     print(hode)
     outfile.write(hode)
-    for feature in layer.getFeatures():
-        objectid += 1
-        #name=feature['id']
-        objhode=f"""
+    objtype = layer.name()
+    if layer.geometryType().name == 'Line':
+        for feature in layer.getFeatures():
+            objectid += 1
+            #name=feature['id']
+            objhode=f"""
 .KURVE {objectid}:
 ..OBJTYPE {objtype}
+..OPPDATERINGSDATO {oppdateringsdato}
+..DATAUTTAKSDATO {datauttaksdato}
 ..NØ"""
-        print(objhode)
-        outfile.write(objhode)
-        for point in feature.geometry().asPolyline():
-            # name=feature['name']
-            x=round(point.x()/unit)
-            y=round(point.y()/unit)
-            outfile.write(f"\n{y} {x}")
+            print(objhode)
+            outfile.write(objhode)
+            for point in feature.geometry().asPolyline():
+                # name=feature['name']
+                x=round(point.x()/unit)
+                y=round(point.y()/unit)
+                outfile.write(f"\n{y} {x}")
+    else:
+        print(f"{layer.geometryType().name} not implemented yet")
+    outfile.write('\n.SLUTT')
 print(f"\n{filename} written")
